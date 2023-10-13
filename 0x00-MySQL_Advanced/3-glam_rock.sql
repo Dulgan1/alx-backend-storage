86166262bd4e39b67bc3b0bf8eae37ca5ced8173
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-13 03:48:28 +0100

    initial commit

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- SQL script that queries the metal_bands table
+-- and lists bands based on Glamor rock main style ordered by longevity
+
+SELECT DISTINCT band_name, IFNULL(split, 2022) - formed as life_span
+FROM metal_bands WHERE FIND_IN_SET('Glam rock', style) ORDER BY lifespan DESC;
