c56bb05d2b20a2960083b3bf0062753d28d241e1
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-13 04:59:36 +0100

    initial commit

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- SQL script creates view:
+-- need_meeting
+CREATE VIEW need_meeting AS
+SELECT student_name
+FROM students
+WHERE score < 80
+   AND (last_meeting IS NULL OR last_meeting < NOW() - INTERVAL 1 MONTH);
+
