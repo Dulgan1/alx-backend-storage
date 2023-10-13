e5acf74e3e5a10326d2d2c84c21c0879e4302903
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-13 04:01:39 +0100

    initial commit

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,5 @@
+-- SQL script creates trigger to decrease no of items after an order
+-- On items table
+
+CREATE TRIGGER item_decrease BEFORE INSERT ON orders FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number WHERE name = NEW.item.name;
