a8cf7a57caf194351bff342511a8eef83b85869d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-18 15:10:11 +0100

    initial commit

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module update"""
+def update_topics(mongo_collection, name, topics):
+    """updates documenst's (school's) topics"""
+    mongo_collection.update_many({"name": name}, {"$sets": {"topics": topics}})
+
+
