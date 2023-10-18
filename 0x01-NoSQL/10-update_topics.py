7a941e5f4a6e298921b52a6a86fae3cb0d879c0d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-18 15:20:54 +0100

    Removed typo

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Module update"""
+
+
 def update_topics(mongo_collection, name, topics):
     """updates documenst's (school's) topics"""
-    mongo_collection.update_many({"name": name}, {"$sets": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
 
 
