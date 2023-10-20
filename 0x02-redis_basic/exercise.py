e0a43c756c2cfd4c997320172dd20a62f38e85b2
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-20 20:51:53 +0100

    initial commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Cache module"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -16,7 +16,25 @@ class Cache:
     def store(self, data: Union[float, int, str, bytes]) -> str:
         """Stores data with key value through redis client instance
         as cache, returns key"""
-        obj_key: uuid.UUID = uuid.uuid1()
+        obj_key: uuid.UUID = uuid.uuid4()
         self._redis.set(str(obj_key), data)
 
         return str(obj_key)
+
+    def get(self, key: str, fn: Callable) -> Union[float, int, str, bytes]:
+        """gets stored data with key as key,
+        uses fn to convert data from bytes to any"""
+        data = self._redis.get(key)
+
+        if fn:
+            data = fn(data)
+
+        return data
+
+    def get_int(self, data: bytes) -> int:
+        """Converts bytes data to int"""
+        return int.from_bytes(data)
+
+    def get_str(self, data: bytes) -> str:
+        """Converts bytes data to str"""
+        return str(data, 'UTF-8')
