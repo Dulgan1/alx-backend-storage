9a03c27e8177c708c127a3751d36a1ff18415e92
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-20 16:43:38 +0100

    ino

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Cache module"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Caching system class"""
+
+    def __init__(self):
+        """Initializes and instance with a redis client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[float, int, str, bytes]) -> str:
+        """Stores data with key value through redis client instance
+        as cache, returns key"""
+        obj_jey = uuid.uuid4()
+        self._redis.set(obj_key, data)
+
+        return obj_key
