f34d74cf3b566814da43cd31216e2fa5e0400c04
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-21 17:36:55 +0100

    decoration

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,6 +25,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data with key value through redis client instance
         as cache, returns key"""
