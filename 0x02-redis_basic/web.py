39bfc82f4a1541d1f59ed98989ab3545775708d4
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-21 22:44:00 +0100

    Decorator

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,12 +3,19 @@
 import requests
 import redis
 
+redis_c = Redis()
+
+
+def keep_count(fn: Callable) -> Callable:
+    """Caches count of fn function call"""
+    def wrapper(arg):
+        key = 'count:' + arg
+        redis_c.incr(key)
+        redis_c.expire(key, 10)
+        return fn(arg)
+    return wrapper
+
+@keep_count
 def get_page(url: str) -> str:
     """Gets content of web page, caches count of webpage gets"""
-    redis_c = redis.Redis()
-    key = 'count:' + url
-    response = requests.get(url).text
-    redis_c.incr(key)
-    redis_c.expire(key, 10)
-    
-    return response
+    return requests.get(url).text
