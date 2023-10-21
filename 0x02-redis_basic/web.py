63d6d33f8dd29b50397ba81e0c607bcfc2067292
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-10-21 23:19:31 +0100

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,24 +2,13 @@
 """Web request caching: redis"""
 import requests
 import redis
-from typing import Callable
 
 redis_c = redis.Redis()
 
 
-def keep_count(fn: Callable) -> Callable:
-    """Caches count of fn function call"""
-    def wrapper(*arg, **kwargs):
-        key = 'count:' + arg
-        redis_c.incr(key)
-        redis_c.expire(key, 10)
-        return fn(*arg, **kwargs)
-    return wrapper
-
-
 def get_page(url: str) -> str:
     """Gets content of web page, caches count of webpage gets"""
-    key = 'count:' + url
+    key = 'count:{{}}'.format(url)
     redis_c.incr(key)
     redis_c.expire(key, 10)
     return requests.get(url).text
